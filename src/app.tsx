132248cd94ac24b5f61372f264337223b6dd5595
Yellow May <kwerep09@yahoo.com>
2021-12-25 11:11:21 +0100

    📦 NEW: initial setup

---
+++ src/app.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+
+export default function App() {
+	return (
+		<>
+			Collections Men Women About Contact Sneaker Company Fall Limited Edition Sneakers These
+			low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer
+			sole, they’ll withstand everything the weather can offer. $125.00 50% $250.00 0 Add to cart
+			<footer className='attribution'>
+				Challenge by
+				<a
+					href='https://www.frontendmentor.io?ref=challenge'
+					target='_blank'
+					rel='noopener noreferrer'>
+					Frontend Mentor
+				</a>
+				. Coded by{' '}
+				<a href='https://yellow-may.vercel.app/' target='_blank' rel='noopener noreferrer'>
+					Yellow May
+				</a>
+				.
+			</footer>
+		</>
+	);
+}
