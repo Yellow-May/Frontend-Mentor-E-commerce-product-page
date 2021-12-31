cabe85c6c2d18262c67bfcd27414d6d37a611d45
Yellow May <kwerep09@yahoo.com>
2021-12-31 12:16:31 +0100

    🐛 FIX: module directory

--- src/app.tsx
+++ src/app.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import Footer from './components/footer';
 import Header from './components/header';
 import ImagePreviewer from './components/image-previewer';
-import ItemInfo from '../components/item-info';
+import ItemInfo from './components/item-info';
 
 export default function App() {
 	return (
